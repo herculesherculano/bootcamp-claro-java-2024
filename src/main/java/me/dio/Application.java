22bb245f0ebb792ab09fcf770f1f372d0124197c
Hercules Herculano <hbheroi@gmail.com>
2024-11-17 16:01:40 -0300

    Initial commit

---
+++ src/main/java/me/dio/Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampClaroJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampClaroJavaSpringbootApplication.class, args);
+	}
+
+}
